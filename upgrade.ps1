b4f6f60b666de854dcdc641fd8521670aaa49bd8
Jenikh <164548559+Jenikh@users.noreply.github.com>
2024-12-29 16:02:55 +0100

    Repair

--- upgrade.ps1
+++ upgrade.ps1
@@ -3,7 +3,7 @@ $version_number_old = [System.IO.File]::ReadAllText("version-number.txt")
 Invoke-WebRequest -Uri "https://jenikh.github.io/En-site/version-number.txt" -OutFile "version-number.txt"
 $version_number_new = [System.IO.File]::ReadAllText("version-number.txt")
 if ($version_number_old == $version_number_new) {
-    exit()
+    exit(1)
 }
 $base_url = "https://jenikh.github.io/En-site/files/en-site.zip"
 Write-Information "Downloading from $base_url"
@@ -14,3 +14,4 @@ Remove-Item "en-site.zip"
 
 
 Write-Host "Upgrade from $version_number_old to $version_number_new copleted!" -BackgroundColor White -ForegroundColor DarkYellow
+exit(0)
