95bb1b2c8fb43bf4b9ab633be0bd07970e724e62
Jenikh <164548559+Jenikh@users.noreply.github.com>
2024-12-29 12:57:27 +0100

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -7,8 +7,6 @@ Invoke-WebRequest -Uri $base_url -OutFile "en-site.zip"
 Expand-Archive -Path "en-site.zip" -DestinationPath ".\" -Force
 Remove-Item "en-site.zip"
 
-Invoke-WebRequest -Uri "https://jenikh.github.io/En-site/new-version.txt" -OutFile "version-number.txt"
+Invoke-WebRequest -Uri "https://jenikh.github.io/En-site/version-number.txt" -OutFile "version-number.txt"
 $version_number_new = [System.IO.File]::ReadAllText("version-number.txt")
 Write-Host "Upgrade from $version_number_old to $version_number_new copleted!" -BackgroundColor White -ForegroundColor DarkYellow
-
-Remove-Item "en-site.zip"
