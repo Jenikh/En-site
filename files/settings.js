170cdb43ab43099c36e34c48646e49b1e1ab568b
Jenikh <164548559+Jenikh@users.noreply.github.com>
2024-12-29 12:47:19 +0100

    Update settings.js

--- files/settings.js
+++ files/settings.js
@@ -1,1 +1,15 @@
+//#region Settings
 
+//#region Words to use
+// Pexeso
+const preklad_words_en = ["hello", "world", "this", "is", "a", "test"];
+const preklad_words_cz = ["ahoj", "svet", "toto", "je", "test"];
+//  None
+
+//#endregion Words to use
+// Pexeso
+const Pexeso_words_cz = ["Ahoj", "Kámen"];
+const Pexeso_words_en = ["Hello", "Stone"];
+
+
+//#endregion Settings
